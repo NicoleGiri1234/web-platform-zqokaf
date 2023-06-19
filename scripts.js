15fc65c83f04b0cca2395ff7959f294acf5d9586
NicoleGiri1234 <127787371+NicoleGiri1234@users.noreply.github.com>
2023-06-19 17:21:17 -0300

    2 files modified

--- scripts.js
+++ scripts.js
@@ -1,6 +1,18 @@
 function doSomethingCool() {
   // alert('WARNING!');
   
-  var x = 'Warning (but in a var)';
-  alert(x);
+ // var x = "Warning via var"
+  
+ // var x = Math.random() * 10;
+ // var y = Math.floor(x);
+// alert(y);
+
+var name = document.querySelector('#name').value;
+// concatenation
+alert(name + ' Is Cool');
+
+var div = document.querySelector('.child');
+var p = document.createElement('p');
+p.textContent = name;
+div.appendChild(p);
 }
